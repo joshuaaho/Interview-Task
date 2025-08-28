a2bc61da648daaf60a091b98cb655943ef92bbcf
Joshua <joshuaa@connect.hku.hk>
2025-08-29 03:51:12 +0800

    fix: use correct parameter type for pickRandomWord

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { WordList } from "./constants";
 
-function pickRandomWord(wordList: WordList): WordList {
+function pickRandomWord(wordList: Record<string, string | number>): WordList {
     const values = Object.keys(wordList);
     const enumKey = values[Math.floor(Math.random() * values.length)];
-    return wordList[enumKey];
+    return wordList[enumKey] as WordList;
 }
+
 
