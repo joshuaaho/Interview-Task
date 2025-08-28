e0242901cd2ada003d8f1353ce367ffe9968df65
Joshua <joshuaa@connect.hku.hk>
2025-08-29 03:19:03 +0800

    feat: initialize game config

---
+++ src/constants.ts
@@ -0,0 +1,25 @@
+export enum WordList {
+  APPLE,
+  BEACH,
+  CHAIR,
+  DREAM,
+  EARTH,
+  FLAME,
+  GRACE,
+  HEART,
+  IRONY,
+  JUICE,
+  KNIFE,
+  LIGHT,
+  MAGIC,
+  NIGHT,
+  OCEAN,
+  PEACE,
+  QUEEN,
+  RIVER,
+  SMILE,
+  TABLE
+}
+
+export const MAX_ATTEMPTS = 6;
+
