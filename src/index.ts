c56b9de92bdd3ae0c525e0c78cd4191be36f56b3
Joshua <joshuaa@connect.hku.hk>
2025-08-29 15:23:35 +0800

    feat: define typescript types for scores

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { WordList } from "./constants";
 
 function pickRandomWord(): WordList {
-    const values = Object.keys(WordList);
-    const enumKey = values[Math.floor(Math.random() * values.length)];
-    return WordList[enumKey] as WordList;
+  const values = Object.keys(WordList);
+  const enumKey = values[Math.floor(Math.random() * values.length)];
+  return WordList[enumKey] as WordList;
 }
 
+type LetterScore = "P" | "M" | "H";
+
+type GuessScore = readonly [LetterScore, LetterScore, LetterScore, LetterScore, LetterScore];
 
