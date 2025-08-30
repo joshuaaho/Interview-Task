0cbc041324139eb82ea71da14bb103f7b3ae6be9
Joshua <joshuaa@connect.hku.hk>
2025-08-30 22:29:07 +0800

    feat: add color constants
    color used to represent H, P and M

--- src/constants.ts
+++ src/constants.ts
@@ -23,3 +23,11 @@ export enum WordList {
 
 export const MAX_ATTEMPTS = 6;
 
+export enum Colors  {
+  RESET = '\x1b[0m',
+  GREY = '\x1b[90m',
+  YELLOW = '\x1b[33m',
+  GREEN = '\x1b[32m',
+}
+
+ 
