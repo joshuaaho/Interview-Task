9df20ea0a6fa179fd88a1f2f854ecea6f6c7e6c3
Joshua <joshuaa@connect.hku.hk>
2025-08-31 23:39:30 +0800

    feat: add handle lost and win states, emit event after client makes a guess

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,28 +1,42 @@
 import { io } from "socket.io-client";
-import { MAX_ATTEMPTS } from "../server/constants";
+import promptSync from "prompt-sync";
 import { formatScore } from "./utils/formatScore";
+import { SERVER_URL } from "./constants";
 
 console.log("Starting client...");
-
+const prompt = promptSync();
 let clientGuess: any = "";
 
 // Connect to the Socket.IO server
-const socket = io("http://localhost:3000");
+const socket = io(SERVER_URL);
 
 // Handle connection
 socket.on("connect", () => {
   console.log("✅ Connected to the server");
 });
 
-// Handle responses from the server
 socket.on("GAME_STARTED", (msg) => {
   clientGuess = prompt("Enter your guess: ");
   socket.emit("GUESS_MADE", clientGuess);
 });
 
 socket.on("GUESS_EVALUATED", (msg) => {
+
   console.log(formatScore(clientGuess, msg));
+
+  // Ask for another guess, game not over at this point
+  clientGuess = prompt("Enter your guess: ");
+  socket.emit("GUESS_MADE", clientGuess);
 });
+
+socket.on("CLIENT_WON", () => {
+  console.log("You won!");
+});
+
+socket.on("CLIENT_LOST", (msg) => {
+  console.log("You lost! The word was: ", msg);
+});
+
 // Handle connection errors
 socket.on("connect_error", (error) => {
   console.log("❌ Connection error:", error.message);
